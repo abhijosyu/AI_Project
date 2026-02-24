d16735c536c2655a865396f7b90a10b9f1b91334
Jaylen Zeng <jaylenzeng@gmail.com>
2026-02-24 17:42:35 -0500

    random move sample

--- test.py
+++ test.py
@@ -8,13 +8,18 @@ print("Environment passed all checks!")
 
 obs, _ = env.reset()
 
-
-for _ in range(60):
-    obs, reward, done, _, info = env.step(0)  # do nothing, let bird fall
-    print(f"bird_y={obs[0]:.3f}, speed={obs[1]:.3f}, pipe_dist={obs[2]:.3f}, gap_top={obs[3]:.3f}, gap_bot={obs[4]:.3f}")
-    if done:
-        break
-
-
+for episode in range(100):
+    obs, _ = env.reset()
+    total_reward = 0
+    steps = 0
+    
+    while True:
+        action = env.action_space.sample()  # random 0 or 1
+        obs, reward, done, _, info = env.step(action)
+        total_reward += reward
+        steps += 1
+        if done:
+            print(f"Episode {episode}: score={info['score']}, steps={steps}, reward={total_reward:.1f}")
+            break
 
 env.close()
