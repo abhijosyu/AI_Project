b44a8cd8e2d0f7b9f3b2dd64e1d1b03632fa2b44
Abhi <abhijosyu@gmail.com>
2026-03-29 22:24:22 +0000

    data for ga

--- train_ga.py
+++ train_ga.py
@@ -3,12 +3,14 @@ import sys
 from flappy_genetic_algo import run_genetic_algo
 from flappy_neural_network import FlappyNeuralNetwork
 from flappy_bird_env import FlappyBirdEnv
+import matplotlib.pyplot as plt
+
 
 train_flag = 'train' in sys.argv
 
 # hyperparameters
 layers = [5, 8, 2]
-population_size = 200
+population_size = 350
 generations = 50
 mutation_rate = 0.1
 
@@ -16,11 +18,26 @@ mutation_rate = 0.1
 if train_flag:
 
     print("Training GA...")
-    best_genome = run_genetic_algo(layers, population_size, generations, mutation_rate)
+    best_genome, best_history, avg_history = run_genetic_algo(layers, population_size, generations, mutation_rate)
 
     # Save the best genome
     np.save("best_genome.npy", best_genome)
+    print(best_genome)
+    print(best_history)
+    print(avg_history)
+
     print("Best genome saved to best_genome.npy")
+
+    # Plot results
+    plt.plot(best_history, label="Best Fitness")
+    plt.plot(avg_history, label="Average Fitness")
+    plt.xlabel("Generation")
+    plt.ylabel("Fitness")
+    plt.title("Genetic Algorithm Performance")
+    plt.legend()
+    plt.show()
+    title = f"ga_performance pop_{population_size}_gen_{generations}_mut_{mutation_rate}.png" 
+    plt.savefig(title, dpi=300)
 
 # Watch it play
 else: 
